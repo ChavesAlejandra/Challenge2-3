3cf5501fd89086da44802e666a21d9c8625a74e3
patricia <141490676+pxtricixdev@users.noreply.github.com>
2024-02-11 18:30:09 +0100

    Class updated.

--- Gestion_Electrodomesticos/Electrical_Appliance.java
+++ Gestion_Electrodomesticos/Electrical_Appliance.java
@@ -1,6 +1,6 @@
 package Gestion_Electrodomesticos;
 
-public class Electrical_Appliance {
+public class ElectricalAppliance {
     private double _price;
     private String _color;
     private char _energy;
@@ -10,28 +10,28 @@ public class Electrical_Appliance {
         return _price;
     }
     private void setPrice (double pPrice) {
-        this._price = pPrice;
+        _price = pPrice;
     }
 
     public String getColor() {
         return _color;
     }
     private void setColor (String pColor) {
-        this._color = pColor;
+        _color = pColor;
     }
 
     public char getEnergy() {
         return _energy;
     }
     private void setEnergy (char pEnergy) {
-        this._energy = pEnergy;
+        _energy = pEnergy;
     }
 
     public double getWeight() {
         return _weight;
     }
     private void setWeight (double pWeight) {
-        this._weight = pWeight;
+        _weight = pWeight;
     }
 
     private final static double PRICE_DEFAULT = 100;
@@ -41,18 +41,18 @@ public class Electrical_Appliance {
 
     //constructor
     public Electrical_Appliance(double pPrice, String pColor, char pEnergy, double pWeight) {
-        this._price = pPrice;
-        this._color = pColor;
-        this._energy = pEnergy;
-        this._weight = pWeight;
+        _price = pPrice;
+        _color = pColor;
+        _energy = pEnergy;
+        _weight = pWeight;
     }
 
     //constructor por defecto
     public Electrical_Appliance () {
-        this._price = PRICE_DEFAULT;
-        this._color = COLOR_DEFAULT;
-        this._energy = ENERGY_DEFAULT;
-        this._weight = WEIGHT_DEFAULT;
+        _price = PRICE_DEFAULT;
+        _color = COLOR_DEFAULT;
+        _energy = ENERGY_DEFAULT;
+        _weight = WEIGHT_DEFAULT;
     }
 
 }
